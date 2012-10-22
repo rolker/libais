1f9725342bb8f9e4a2cd8fed754aa88f14e1b1a5
Kurt Schwehr <schwehr@google.com>
2012-10-22 10:46:41 -0700

    python decoding of ais 8 1 26
    Conflicts:
    	ais_py.cpp
    	setup.py

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ ais_module = Extension('ais',
                                'ais7_13.cpp',
                                'ais8.cpp', # Broadcast binary message (BBM)
                                'ais8_001_22.cpp', # Area notice
+                               'ais8_001_26.cpp', # Environmental Sensor Report
                                'ais8_200_10.cpp', # Inland ENCs
-                               'ais8_366_22.cpp',
                                'ais9.cpp',
                                'ais10.cpp', # :
                                # 11 See 4 - ;
